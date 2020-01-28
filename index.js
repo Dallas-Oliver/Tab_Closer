8e1223bf7065fa2a1b0b5babf1f687c475614e20
Dallas <dallas.oliver91@gmail.com>
2020-01-28 17:18:42 -0500

    finished

---
+++ index.js
@@ -0,0 +1,103 @@
+window.addEventListener("DOMContentLoaded", function() {
+  const closeAll = document.querySelector("#close-all");
+  closeAll.addEventListener("click", closeAllTabs);
+
+  const refresh = document.getElementById("refresh");
+  refresh.addEventListener("click", refreshSession);
+
+  const choose = document.getElementById("choose-tabs");
+  choose.addEventListener("click", chooseTabsToKeepOpen);
+});
+
+function closeAllTabs() {
+  chrome.windows.getCurrent({ populate: true }, function(window) {
+    tabsToClose = window.tabs.map(tab => {
+      return tab.id;
+    });
+
+    closeTabs(tabsToClose);
+  });
+}
+
+async function refreshSession() {
+  let newTabId = openNewTab();
+  let tabsToClose;
+
+  chrome.windows.getCurrent({ populate: true }, function(window) {
+    tabsToClose = window.tabs.map(tab => {
+      if (tab.id !== newTabId) {
+        return tab.id;
+      }
+    });
+
+    closeTabs(tabsToClose);
+  });
+}
+
+function chooseTabsToKeepOpen() {
+  let tabsToKeep = [];
+
+  chrome.windows.getCurrent({ populate: true }, function(window) {
+    window.tabs.map(tab => {
+      let li = document.createElement("li");
+      li.textContent = tab.title;
+      li.id = tab.title;
+      li.classList.add("item");
+
+      li.addEventListener("click", function() {
+        if (tabsToKeep.indexOf(tab.id) === -1) {
+          tabsToKeep.push(tab.id);
+          li.classList.add("marked");
+        } else {
+          tabsToKeep.splice(tabsToKeep.indexOf(tab.id), 1);
+          li.classList.remove("marked");
+        }
+      });
+
+      let chooseList = document.getElementById("choose-list");
+      chooseList.appendChild(li);
+    });
+  });
+
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "close tabs";
+  let chooseList = document.getElementById("choose-list");
+  chooseList.append(deleteButton);
+  deleteButton.classList.add("btn");
+  deleteButton.classList.add("btn-info");
+
+  deleteButton.addEventListener(
+    "click",
+    async () => await tabSelector(tabsToKeep)
+  );
+
+  return tabsToKeep;
+}
+
+function tabSelector(tabsToKeep) {
+  let tabsToDelete = [];
+  chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, tabs => {
+    tabs.forEach(tab => {
+      if (!tabsToKeep.includes(tab.id)) {
+        tabsToDelete.push(tab.id);
+      }
+    });
+    closeTabs(tabsToDelete);
+  });
+}
+
+function closeTabs(tabIds) {
+  console.log(tabIds);
+
+  chrome.tabs.remove(tabIds, function() {
+    console.log(tabIds);
+  });
+}
+
+function openNewTab() {
+  chrome.windows.getCurrent({ populate: true }, function(window) {
+    chrome.tabs.create({ active: false }, function(tab) {
+      return tab.id;
+    });
+  });
+}
